a036af7d49d3a57b3cf51d0825087118c3ac98ca
Senthil Kumar <senthilkumar.cse04@gmail.com>
2017-06-28 10:35:25 +0530

    refactored config

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,9 @@
 var env = process.env.NODE_ENV || 'development';
 console.log(`${env} environment`);
-if(env === 'development'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = "mongodb://localhost:27017/NotesApp";
-}
-else if (env === 'test'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = "mongodb://localhost:27017/NotesAppTest";
+if (env === "development" || env === "test") {
+    var config = require('./config.json');
+    var configEnv = config[env];
+    Object.keys(configEnv).forEach((key)=>{
+        process.env[key] = configEnv[key];
+    })
 }
